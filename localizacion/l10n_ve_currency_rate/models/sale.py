ecb12a100cc7d878b6ed59d7ae78c359b4b1f5c5
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-12-12 13:42:02 -0400

    prueba v4

--- localizacion/l10n_ve_currency_rate/models/sale.py
+++ localizacion/l10n_ve_currency_rate/models/sale.py
@@ -79,7 +79,7 @@ class SaleOrderLine(models.Model):
                     'price_subtotal_rate': (line.price_subtotal * line.order_id.rate),
                 })
             if line.order_id.company_id.currency_id == line.order_id.currency_id:
-                #line.price_unit=line.order_id.rate*line.product_id.list_price_usd
+                line.price_unit=line.order_id.rate*line.product_id.list_price_usd
                 if line.order_id.rate:
                     line.update({
                         'price_unit_rate': (line.price_unit / line.order_id.rate),
