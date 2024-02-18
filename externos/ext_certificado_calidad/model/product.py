795b1b53bde41a377282a095ef5aa9e99bae7ebd
dsojo1980 <dsojo.tanfe@gmail.com>
2024-02-18 14:55:21 -0400

    Ajustes de certificado calidad

--- externos/ext_certificado_calidad/model/product.py
+++ externos/ext_certificado_calidad/model/product.py
@@ -11,6 +11,15 @@ class ProductTemplate(models.Model):
     gramaje = fields.Char()
     adhesivo = fields.Char()
 
+    def write(self,vals):
+        super().write(vals)
+        lista=self.env['product.product'].search([('product_tmpl_id','=',self.id)])
+        if lista:
+            for det in lista:
+                det.espesor=self.espesor
+                det.gramaje=self.gramaje
+                det.adhesivo=self.adhesivo
+
 class ProductTemplate(models.Model):
     _inherit = 'product.product'
     
